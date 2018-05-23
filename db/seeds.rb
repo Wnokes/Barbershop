790c4a6cc7943d795857c782bfcdde7c498d5c82
andrewlloyd0129 <andrewlloyd0129@gmail.com>
2018-05-23 10:54:34 -0600

    added admin and user to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,3 +17,21 @@ ContactInfo.create!(
 		scheduler: "google.com",
 		about_me: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
 	)
+
+
+User.create!(
+   email: "admin@test.com",
+   password: "asdfasdf",
+   password_confirmation: "asdfasdf",
+   roles: "admin"
+)
+
+puts "1 admin user created"
+
+User.create!(
+   email: "test@test.com",
+   password: "asdfasdf",
+   password_confirmation: "asdfasdf"
+)
+
+puts "1 regular user created"
