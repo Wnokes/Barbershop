c40925c17cf7c2333abd8e7227c6bc5d7bb9d2ce
andrewlloyd0129 <andrewlloyd0129@gmail.com>
2018-05-23 10:44:05 -0600

    droped devise MODEL table, added USER table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180523163808) do
+ActiveRecord::Schema.define(version: 20180523164304) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,7 +40,17 @@ ActiveRecord::Schema.define(version: 20180523163808) do
     t.index ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
   end
 
-  create_table "models", force: :cascade do |t|
+  create_table "portfolios", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "thumbnail_image"
+    t.string "main_image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "slug"
+  end
+
+  create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -53,18 +63,8 @@ ActiveRecord::Schema.define(version: 20180523163808) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_models_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
-  end
-
-  create_table "portfolios", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
-    t.string "thumbnail_image"
-    t.string "main_image"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "slug"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
