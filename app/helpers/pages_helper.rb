d2b7c84bf1ea80cd626cf69ce60efd734cbd00cd
Wnokes <westonroynokes@gmail.com>
2018-05-29 13:42:47 -0600

    added a favicon

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -3,10 +3,10 @@ module PagesHelper
 
 		str = '<div class="col-md-2 center-text card contact">'
 		str += "<div class='row justify-content-md-center'>"
-		str += "<div class='col-md-12'>#{link_to((fa_icon 'facebook',text: 'FaceBook'), @contact.facebook, class: 'social-link link body-font')}</div>" if strcheck @contact.facebook
-		str += "<div class='col-md-12'>#{link_to((fa_icon 'camera-retro',text: 'Instagram'), @contact.instagram, class: "social-link link body-font")}</div>" if strcheck @contact.instagram
-		str += "<div class='col-md-12'>#{link_to((fa_icon 'twitter',text: 'Twitter'), @contact.twitter, class: "social-link link body-font")}</div>" if strcheck @contact.twitter
-		str += "<div class='col-md-12'>#{link_to((fa_icon 'linkedin',text: 'LinkedIn'), @contact.linkedin, class: "social-link link body-font")}</div>" if strcheck @contact.linkedin
+		str += "<div class='col-md-12'>#{link_to((fa_icon 'facebook',text: 'FaceBook'), @contact.facebook, class: 'social-link link body-font', target: :_blank)}</div>" if strcheck @contact.facebook
+		str += "<div class='col-md-12'>#{link_to((fa_icon 'camera-retro',text: 'Instagram'), @contact.instagram, class: "social-link link body-font", target: :_blank)}</div>" if strcheck @contact.instagram
+		str += "<div class='col-md-12'>#{link_to((fa_icon 'twitter',text: 'Twitter'), @contact.twitter, class: "social-link link body-font", target: :_blank)}</div>" if strcheck @contact.twitter
+		str += "<div class='col-md-12'>#{link_to((fa_icon 'linkedin',text: 'LinkedIn'), @contact.linkedin, class: "social-link link body-font", target: :_blank)}</div>" if strcheck @contact.linkedin
 
 		str += "</div>"	
 		str += link_to("Edit Contact and About", edit_contact_info_path(@contact)) if (logged_in?(:admin))
